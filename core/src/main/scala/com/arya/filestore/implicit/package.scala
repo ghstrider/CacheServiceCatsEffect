13ef2c660b8b211ca4b57b4930e62e259c00b738
ghstrider <ghstrider@users.noreply.github.com>
2025-09-10 23:51:34 +0530

    Change file store create to home dir

--- core/src/main/scala/com/arya/filestore/implicit/package.scala
+++ core/src/main/scala/com/arya/filestore/implicit/package.scala
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
 package object `implicit` {
-  // Use TTL-aware file store
-  implicit val fs2File: Fs2FileKeyValueStoreTTL[IO, String, String] = new Fs2FileKeyValueStoreTTL[IO, String, String]("filestore.kv", System.getProperty("user.dir"))
+  // Use TTL-aware file store in user home directory
+  implicit val fs2File: Fs2FileKeyValueStoreTTL[IO, String, String] = new Fs2FileKeyValueStoreTTL[IO, String, String]("filestore.kv", System.getProperty("user.home"))
   implicit val kv: KeyValueStore[IO, String, String] = fs2File
 }
